4cc288fbe38c0e3f105122f46e3d9f61d2ea8192
bmeakings <b.meakings@gmail.com>
2023-03-21 22:55:14 +0000

    Updated 21/03/2023

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const {app, BrowserWindow, Menu, ipcMain, dialog} = require('electron');
-const {autoUpdater} = require('electron-updater');
+const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron');
 
 let win = null;
-let installUpdate = false;
-
-autoUpdater.autoDownload = false;
 
 function createAppWindow() {
 	win = new BrowserWindow({
@@ -34,58 +30,7 @@ function createAppWindow() {
 function createAppMenu() {
 	let menu = null;
 	let menuTemplate = [];
-	let fileSubMenu = [];
-	let viewSubMenu = [];
-	let helpSubMenu = [];
-/*
-	viewSubMenu.push({
-		label: 'Debugging Tools',
-		click: function() {
-			win.webContents.toggleDevTools();
-		},
-	});
 
-	if (process.platform != 'darwin') {
-		helpSubMenu.push({
-			label: 'About',
-			role: 'about',
-		});
-
-		helpSubMenu.push({
-			label: 'Check for Update',
-			click: function() {
-				autoUpdater.checkForUpdates();
-			},
-		});
-	}
-
-	if (process.platform == 'darwin') {
-		menuTemplate.push({
-			label: app.getName(),
-			submenu: [
-				{
-					label: 'About',
-					role: 'about',
-				},
-				{
-					label: 'Check for Update',
-					click: function() {
-						autoUpdater.checkForUpdates();
-					},
-				},
-			],
-		});
-	}
-
-	if (fileSubMenu.length > 0)
-		menuTemplate.push({label: 'File', submenu: fileSubMenu});
-
-	if (viewSubMenu.length > 0)
-		menuTemplate.push({label: 'View', submenu: viewSubMenu});
-
-	if (helpSubMenu.length > 0)
-		menuTemplate.push({label: 'Help', submenu: helpSubMenu});
-*/
 	menu = Menu.buildFromTemplate(menuTemplate);
 
 	Menu.setApplicationMenu(menu);
@@ -102,58 +47,4 @@ app.on('window-all-closed', function() {
 
 ipcMain.on('exitApp', function() {
 	win.close();
-})
-
-/*
- * app updater events
- */
-
-ipcMain.on('check_updates', function() {
-	autoUpdater.checkForUpdates();
-});
-
-ipcMain.on('download_update', function() {
-	installUpdate = false;
-
-	autoUpdater.downloadUpdate();
-	win.webContents.send('update_downloading', false);
-});
-
-ipcMain.on('install_update', function() {
-	installUpdate = true;
-
-	autoUpdater.downloadUpdate();
-	win.webContents.send('update_downloading', true);
-});
-
-autoUpdater.on('error', function(error) {
-	win.webContents.send('update_error', error);
-});
-
-autoUpdater.on('checking-for-update', function() {
-	win.webContents.send('update_checking', false);
-});
-
-autoUpdater.on('update-available', function() {
-	win.webContents.send('update_notify', true);
-});
-
-autoUpdater.on('update-not-available', function() {
-	win.webContents.send('update_notify', false);
-});
-
-autoUpdater.on('update-downloaded', function() {
-	if (!installUpdate)
-		return;
-
-	app.removeAllListeners('window-all-closed');
-	win.close();
-
-	setTimeout(function() {
-		autoUpdater.quitAndInstall();
-	}, 5000);
-});
-
-autoUpdater.on('download-progress', function(progress) {
-	win.webContents.send('update_progress', progress);
 });
